2eae9e92787b571485d28e6bc2a11a1b12b7ad6c
Amin Mir <aminmir326@gmail.com>
2026-02-10 16:45:16 +0200

    Update nvim config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,18 @@ end
 
 fish_add_path /usr/local/go/bin
 set fish_prompt_pwd_dir_length 0
+
+alias fd fdfind
+
+ set -gx PATH \
+     $HOME/.local/bin \
+     /snap/bin \
+     /opt/zig \
+     /usr/local/go/bin \
+     $HOME/.cargo/bin \
+     /usr/local/sbin \
+     /usr/local/bin \
+     /usr/sbin \
+     /usr/bin \
+     /sbin \
+     /bin
